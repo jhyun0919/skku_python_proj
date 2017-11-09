f69be0b9e2931d0e17a67c7af82b1393fff7a515
Park Jee Hyun <jhyun19@gmail.com>
2017-11-09 16:17:22 +0900

    term_proj

--- codes/utils.py
+++ codes/utils.py
@@ -2,7 +2,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import datetime
 import numpy as np
+import pandas as pd
 import locale
+import plotly.graph_objs as go
+from sklearn import cluster, covariance, manifold
+from matplotlib.collections import LineCollection
+import matplotlib.pyplot as plt
 
 __author__ = "Park Jee Hyun"
 __copyright__ = "N.A."
@@ -111,10 +116,146 @@ class DataDecorator:
     def __init__(self):
         pass
 
+    @staticmethod
+    def set_dataframe(idx, quotes):
+        market_dates = np.vstack([q['date'] for q in quotes])
+        open_prices = np.vstack([q['open'] for q in quotes])
+        high_prices = np.vstack([q['high'] for q in quotes])
+        low_prices = np.vstack([q['low'] for q in quotes])
+        close_prices = np.vstack([q['close'] for q in quotes])
+        volume = np.vstack([q['volume'] for q in quotes])
+
+        data_dictionary = {'market_dates': list(market_dates[idx]),
+                           'open_prices': list(open_prices[idx]),
+                           'high_prices': list(high_prices[idx]),
+                           'low_prices': list(low_prices[idx]),
+                           'close_prices': list(close_prices[idx]),
+                           'volume': list(volume[idx])}
+        df = pd.DataFrame(data_dictionary)
+        df = df.sort_values(by='market_dates')
+        df = df.set_index('market_dates')
+
+        return df
+
+    @staticmethod
+    def set_candelstick_data(df):
+        trace = go.Candlestick(x=df.index,
+                               open=df.open_prices,
+                               high=df.high_prices,
+                               low=df.low_prices,
+                               close=df.close_prices)
+        return [trace]
+
+    @staticmethod
+    def show_cluster(edge_model, embedding, names, n_labels, labels):
+        # Visualization
+        plt.figure(1, facecolor='w', figsize=(10, 8))
+        plt.clf()
+        ax = plt.axes([0., 0., 1., 1.])
+        plt.axis('off')
+
+        # Display a graph of the partial correlations
+        partial_correlations = edge_model.precision_.copy()
+        d = 1 / np.sqrt(np.diag(partial_correlations))
+        partial_correlations *= d
+        partial_correlations *= d[:, np.newaxis]
+        non_zero = (np.abs(np.triu(partial_correlations, k=1)) > 0.02)
+
+        # Plot the nodes using the coordinates of our embedding
+        plt.scatter(embedding[0], embedding[1], s=100 * d ** 2, c=labels,
+                    cmap=plt.cm.spectral)
+
+        # Plot the edges
+        start_idx, end_idx = np.where(non_zero)
+        # a sequence of (*line0*, *line1*, *line2*), where::
+        #            linen = (x0, y0), (x1, y1), ... (xm, ym)
+        segments = [[embedding[:, start], embedding[:, stop]]
+                    for start, stop in zip(start_idx, end_idx)]
+        values = np.abs(partial_correlations[non_zero])
+        lc = LineCollection(segments,
+                            zorder=0, cmap=plt.cm.hot_r,
+                            norm=plt.Normalize(0, .7 * values.max()))
+        lc.set_array(values)
+        lc.set_linewidths(15 * values)
+        ax.add_collection(lc)
+
+        # Add a label to each node. The challenge here is that we want to
+        # position the labels to avoid overlap with other labels
+        for index, (name, label, (x, y)) in enumerate(
+                zip(names, labels, embedding.T)):
+
+            dx = x - embedding[0]
+            dx[index] = 1
+            dy = y - embedding[1]
+            dy[index] = 1
+            this_dx = dx[np.argmin(np.abs(dy))]
+            this_dy = dy[np.argmin(np.abs(dx))]
+            if this_dx > 0:
+                horizontalalignment = 'left'
+                x = x + .002
+            else:
+                horizontalalignment = 'right'
+                x = x - .002
+            if this_dy > 0:
+                verticalalignment = 'bottom'
+                y = y + .002
+            else:
+                verticalalignment = 'top'
+                y = y - .002
+            plt.text(x, y, name, size=10,
+                     horizontalalignment=horizontalalignment,
+                     verticalalignment=verticalalignment,
+                     bbox=dict(facecolor='w',
+                               edgecolor=plt.cm.spectral(label / float(n_labels)),
+                               alpha=.6))
+
+        plt.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
+                 embedding[0].max() + .10 * embedding[0].ptp(), )
+        plt.ylim(embedding[1].min() - .03 * embedding[1].ptp(),
+                 embedding[1].max() + .03 * embedding[1].ptp())
+
+        plt.show()
+
 
 class Cluster:
     def __init__(self):
         pass
+
+    @staticmethod
+    def affinity(quotes):
+        # the daily variations of the quotes
+        open_prices = np.vstack([q['open'] for q in quotes])
+        close_prices = np.vstack([q['close'] for q in quotes])
+        variation = close_prices - open_prices
+        # the names
+        names = FinanceDataParser().get_names()
+
+        # Learn a graphical structure from the correlations
+        edge_model = covariance.GraphLassoCV()
+        # standardize the time series: using correlations rather than covariance
+        # is more efficient for structure recovery
+        X = variation.copy().T
+        X /= X.std(axis=0)
+        edge_model.fit(X)
+
+        # Cluster using affinity propagation
+        _, labels = cluster.affinity_propagation(edge_model.covariance_)
+        n_labels = labels.max()
+        for i in range(n_labels + 1):
+            print('Cluster %i: %s' % ((i + 1), ', '.join(names[labels == i])))
+
+        # the nodes (the stocks) on a 2D plane
+
+        # We use a dense eigen_solver to achieve reproducibility (arpack is
+        # initiated with random vectors that we don't control). In addition, we
+        # use a large number of neighbors to capture the large-scale structure.
+        node_position_model = manifold.LocallyLinearEmbedding(n_components=2,
+                                                              eigen_solver='dense',
+                                                              n_neighbors=6)
+
+        embedding = node_position_model.fit_transform(X.T).T
+
+        return edge_model, embedding, names, n_labels, labels
 
 
 if __name__ == "__main__":
